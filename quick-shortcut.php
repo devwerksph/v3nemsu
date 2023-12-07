4c5dc4c6def3683fde522eb42edb941e7c8477c8
devwerksph <148300238+devwerksph@users.noreply.github.com>
2023-12-07 14:12:31 +0800

    added quick short cut php

---
+++ quick-shortcut.php
@@ -0,0 +1,261 @@
+<?php include 'process/execute_auth_.php'; ?>
+<?php include 'header.php'; ?> <!-- Header Initialize Links -->
+<body>
+
+<?php include 'head.php'; ?><!-- Head Nav -->
+
+    <div class="container-fluid">
+      <div class="row">
+
+        <?php // include 'side-nav.php'; ?> <!-- Side Nav -->
+
+       <!-- <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">-->
+        <div class="col-md-12">
+
+          <h2 class="sub-header text-center">Quick Shortcut</h2>
+          <div class="container">
+            <br/>
+
+
+
+            <!-- quick shortcut-->
+
+
+	<style>
+        .panel-enrollment{background:#337ab7;color:#fff;}
+        a{color:#fff;} a:hover{color:#fff;}
+        .panel-hr{background:#3CB371;}
+        .panel-alumni{background: #c1220c;}
+        .panel-accounting{background:#f9d77e;}
+        .panel-procure{background:#896B60;}
+    </style>
+	<article class="wrapper">
+	    
+	    
+	    <section class="main">
+	        
+	        <section class="tab-content">
+	            
+	           <section class="tab-pane active fade in content" id="dashboard">
+	               
+	                <div class="row">
+	                   
+                        <div class="col-xs-3">
+	                        <div class="panel panel-primary panel-enrollment">
+	                            <div class="panel-body">
+                                    <a href="#"><h4><i class="bi bi-person-plus bi-margin"></i>Admission</h4></a>
+	                                <br/><br/>
+	                            </div>
+	                        </div>
+	                    </div>
+	                    
+	                    <div class="col-xs-3">
+	                        <div class="panel panel-success panel-hr">
+	                            <div class="panel-body">
+                                <a href="#"><h4><i class="bi bi-people bi-margin"></i> Human Resource</h4></a>
+	                                <br/><br/>
+	                            </div>
+	                        </div>
+	                    </div>
+	                    
+                        <div class="col-xs-3">
+	                        <div class="panel panel-danger panel-alumni">
+	                            <div class="panel-body">
+                                <a href=""><h4><i class="bi bi-mortarboard bi-margin"></i> Alumni</h4></a>
+	                                <br/><br/>
+	                            </div>
+	                        </div>
+	                    </div>
+
+                        <div class="col-xs-3">
+	                        <div class="panel panel-warning panel-accounting">
+	                            <div class="panel-body">
+                                <a href=""><h4><i class="bi bi-calculator bi-margin"></i> Accounting</h4></a>
+	                                <br/><br/>
+	                            </div>
+	                        </div>
+	                    </div>
+
+	                    <div class="col-xs-3">
+	                        <div class="panel panel-danger panel-procure">
+	                            <div class="panel-body">
+                                <a href=""><h4><i class="bi bi-bag-check bi-margin"></i>Procurement</h4></a>
+	                                <br/><br/>
+	                            </div>
+	                        </div>
+	                    </div>
+
+                        <div class="col-xs-3">
+	                        <div class="panel panel-danger ">
+	                            <div class="panel-body">
+                                <a href=""><h4><i class="bi bi-bag-check bi-margin"></i>Procurement</h4></a>
+	                                <br/><br/>
+	                            </div>
+	                        </div>
+	                    </div>
+	                    
+	                   <div class="col-md-12"><hr></div>
+	                   
+	                   <div class="col-xs-12 col-sm-9">
+	                       <div class="panel panel-success">
+	                           <div class="panel-heading">
+	                               Message 
+	                           </div>
+	                           <div class="panel-body">
+	                               This layout uses tabs to demonstrate what you could do with it. It probably makes more sense to use individual pages/templates in a production app.
+	                               <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+	                           </div>
+	                       </div>
+	                   </div>
+	                   
+	                   <div class="col-xs-12 col-sm-3">
+	                       <div class="panel panel-default">
+	                           <div class="panel-heading">
+	                               Something
+	                           </div>
+	                           <div class="panel-body">
+	                               The sidebar is naturally responsive according to screen width. You can toggle it using the menu button in the topbar. Test it out by increasing/decreasing your screen width and watch it respond.
+	                           </div>
+	                       </div>
+	                       
+	                       <div class="panel panel-default">
+	                           <div class="panel-heading">
+	                              082
+	                           </div>
+	                           <div class="panel-body">
+	                               
+	                           </div>
+	                       </div>
+	                   </div>
+	                   
+	               </div>
+	               
+	           </section>
+	           
+	           <section class="tab-pane fade" id="configuration">
+	               <nav class="subbar">
+			            <ul class="nav nav-tabs">
+				            <li class="active"><a href="#access" data-toggle="tab"><i class="fa fa-code"></i> <span>System</span></a></li>
+				            <li><a href="#roles" data-toggle="tab"><i class="fa fa-user"></i> <span>Roles</span></a></li>
+			            </ul>
+		            </nav>
+		            
+		            <section class="tab-content content">
+		                
+		                <section class="tab-pane active fade in" id="access">
+		                    
+                            <div class="row">
+                                <div class="col-xs-12">
+	                                <div class="panel panel-default">
+	                                    <div class="panel-heading">
+	                                        Something
+	                                    </div>
+	                                    <div class="panel-body">
+	                                        <br/><br/><br/><br/>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                            
+	                            <div class="col-xs-12 col-sm-4">
+	                                <div class="panel panel-default">
+	                                    <div class="panel-heading">
+	                                        Something
+	                                    </div>
+	                                    <div class="panel-body">
+	                                        <br/><br/><br/><br/>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                            
+	                            <div class="col-xs-12 col-sm-4">
+	                                <div class="panel panel-default">
+	                                    <div class="panel-heading">
+	                                        Something
+	                                    </div>
+	                                    <div class="panel-body">
+	                                        <br/><br/><br/><br/>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                            
+	                            <div class="col-xs-12 col-sm-4">
+	                                <div class="panel panel-default">
+	                                    <div class="panel-heading">
+	                                        Something
+	                                    </div>
+	                                    <div class="panel-body">
+	                                        <br/><br/><br/><br/>
+	                                    </div>
+	                                </div>
+	                            </div>
+                            </div>
+		                    
+		                </section>
+		                
+		                <section class="tab-pane fade" id="roles">
+		                    
+		                    <div class="row">
+                                <div class="col-xs-12 col-sm-8 col-md-9">
+	                                <div class="panel panel-default">
+	                                    <div class="panel-heading">
+	                                        Something
+	                                    </div>
+	                                    <div class="panel-body">
+	                                        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                            
+	                            <div class="hidden-xs col-sm-4 col-md-3">
+	                                <div class="panel panel-default">
+	                                    <div class="panel-heading">
+	                                        Something
+	                                    </div>
+	                                    <div class="panel-body">
+	                                        <br/><br/><br/>
+	                                    </div>
+	                                </div>
+	                                
+	                                <div class="panel panel-default">
+	                                    <div class="panel-heading">
+	                                        Something
+	                                    </div>
+	                                    <div class="panel-body">
+	                                        <br/><br/><br/>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                       </div>
+		                </section>
+		                
+		            </section>
+		            
+	           </section>
+	           
+	           <section class="tab-pane fade" id="users">
+	               
+	           </section>
+	           
+	           <section class="tab-pane fade" id="mail">
+	               
+	           </section>
+	           
+	        </section>
+	        
+	        
+	                
+	        
+	    </section>
+	    
+	</article>
+        
+        </div>
+        </div><!--end col-sm-class-->
+        </div><!--end row class-->
+        </div><!--end container-fluid class-->
+           
+          
+
+   <?php include 'footer.php'; ?>
+  </body>
+</html>
